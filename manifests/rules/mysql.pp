851f3ba7daefe2b054a016fd9e7d23bb9194c912
Pietro Monteiro <pietro@deckmonitoring.com>
2011-03-30 11:36:51 -0700

    Make mysql rule zone configurable.

--- manifests/rules/mysql.pp
+++ manifests/rules/mysql.pp
@@ -1,11 +1,13 @@
 class shorewall::rules::mysql {
-	shorewall::rule {
-		'net-me-tcp_mysql':
-            source          => 'net',
-            destination     => '$FW',
-            proto           => 'tcp',
-            destinationport => '3306',
-            order           => 240,
-            action          => 'ACCEPT';
-	}
-}
+  $mysql_zone ? { '' => 'net', default => $mysql_zone }
+
+  shorewall::rule {
+    "$mysql_zone-me-tcp_mysql":
+      source          => "$mysql_zone",
+      destination     => '$FW',
+      proto           => 'tcp',
+      destinationport => '3306',
+      order           => 240,
+      action          => 'ACCEPT';
+  }
+ 
