56d4653de912d5969c9e25d403d268f2384db3a1
Pietro Monteiro <pietro@deckmonitoring.com>
2011-03-29 17:52:29 -0700

    Update the name of the munin rule accordingly.

--- manifests/rules/munin.pp
+++ manifests/rules/munin.pp
@@ -2,7 +2,7 @@ class shorewall::rules::munin {
     shorewall::params { 'MUNINPORT': value => $munin_port ? { '' => 4949, default => $munin_port } }
     shorewall::params { 'MUNINCOLLECTOR': value => $munin_collector ? { '' => '127.0.0.1', default => $munin_collector } }
     shorewall::params { 'MUNINCOLLECTORZONE': value => $munin_collector_zone ? { '' => 'net', default => $munin_collector_zone } }
-    shorewall::rule{'net-me-munin-tcp':
+    shorewall::rule{" $munin_collector_zone-me-munin-tcp":
         source          => '$MUNINCOLLECTORZONE:$MUNINCOLLECTOR',
         destination     => '$FW',
         proto           => 'tcp',
