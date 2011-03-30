a3a3ee306ce0660b56029d3d7d10ce8f74455c65
Pietro Monteiro <pietro@deckmonitoring.com>
2011-03-29 17:48:18 -0700

    Make the munin collector zone configurable.

--- manifests/rules/munin.pp
+++ manifests/rules/munin.pp
@@ -1,8 +1,9 @@
 class shorewall::rules::munin {
     shorewall::params { 'MUNINPORT': value => $munin_port ? { '' => 4949, default => $munin_port } }
     shorewall::params { 'MUNINCOLLECTOR': value => $munin_collector ? { '' => '127.0.0.1', default => $munin_collector } }
+    shorewall::params { 'MUNINCOLLECTORZONE': value => $munin_collector_zone ? { '' => 'net', default => $munin_collector_zone } }
     shorewall::rule{'net-me-munin-tcp':
-        source          => 'net:$MUNINCOLLECTOR',
+        source          => '$MUNINCOLLECTORZONE:$MUNINCOLLECTOR',
         destination     => '$FW',
         proto           => 'tcp',
         destinationport => '$MUNINPORT',
