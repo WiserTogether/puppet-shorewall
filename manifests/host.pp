ce6bbe28cd829708a1918b7a03d8ba068a0606ba
Erik LaBianca <erik.labianca@gmail.com>
2012-03-18 22:34:00 -0400

    add nickname to hosts.pp allowing CIDR netmasks to be used in host names

--- manifests/host.pp
+++ manifests/host.pp
@@ -1,9 +1,10 @@
 define shorewall::host(
     $zone,
+    $nickname=$title,
     $options = 'tcpflags,blacklist,norfc1918',
     $order='100'
 ){
-    shorewall::entry{"hosts.d/${order}-${title}":
+    shorewall::entry{"hosts.d/${order}-${nickname}":
         line => "${zone} ${name} ${options}"
     }
 }
